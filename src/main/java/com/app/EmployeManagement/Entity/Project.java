107c436325b8cb0a018cec7fdf569ca79a4aab10
Tombosoa <hei.nantenaina.4@gmail.com>
2024-02-02 18:49:45 +0300

    feat: add a changes

--- src/main/java/com/app/EmployeManagement/Entity/Project.java
+++ src/main/java/com/app/EmployeManagement/Entity/Project.java
@@ -16,12 +16,14 @@ public class Project {
     private String name;
     private Time duration;
     private Timestamp start_date;
+    private boolean status;
 
-    public Project(int id, String name, Time duration, Timestamp start_date) {
+    public Project(int id, String name, Time duration, Timestamp start_date, boolean status) {
         this.id = id;
         this.name = name;
         this.duration = duration;
         this.start_date = start_date;
+        this.status = status;
     }
 
     public Project() {
@@ -59,16 +61,24 @@ public class Project {
         this.start_date = start_date;
     }
 
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Project project)) return false;
-        return getId() == project.getId() && Objects.equals(getName(), project.getName()) && Objects.equals(getDuration(), project.getDuration()) && Objects.equals(getStart_date(), project.getStart_date());
+        return getId() == project.getId() && isStatus() == project.isStatus() && Objects.equals(getName(), project.getName()) && Objects.equals(getDuration(), project.getDuration()) && Objects.equals(getStart_date(), project.getStart_date());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getName(), getDuration(), getStart_date());
+        return Objects.hash(getId(), getName(), getDuration(), getStart_date(), isStatus());
     }
 
     @Override
@@ -78,6 +88,7 @@ public class Project {
                 ", name='" + name + '\'' +
                 ", duration=" + duration +
                 ", start_date=" + start_date +
+                ", status=" + status +
                 '}';
     }
 }
--- src/main/resources/Scripts/create_table_salary.sql
+++ src/main/resources/Scripts/create_table_salary.sql
@@ -2,5 +2,6 @@ create table salary(
 id serial primary key,
 amount double precision,
 riseup double precision,
-paiement_date timestamp with time zone
+paiement_date timestamp with time zone,
+status boolean default false
 );
