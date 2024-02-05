644d40c731a838eb7bfa7146fc89a92dccfbb728
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-02-05 15:12:48 +0300

    feat: implement user credentials

--- src/main/java/com/app/EmployeManagement/Entity/UserCredentials.java
+++ src/main/java/com/app/EmployeManagement/Entity/UserCredentials.java
@@ -1,2 +1,39 @@
-package com.app.EmployeManagement.Entity;public class UserCredentials {
+
+package com.app.EmployeManagement.Entity;
+
+public class UserCredentials {
+    private String email;
+    private String password;
+
+    public UserCredentials(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserCredentials() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserCredentials{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
